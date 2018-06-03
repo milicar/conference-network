59045c21d699d437e268cc5b3e982eb084ccc3e8
Milica Rudic <milicarudic@gmail.com>
2018-06-03 23:59:23 +0200

    Parsing tweets to graph-like structure.

--- src/conference_network/models/tweets.clj
+++ src/conference_network/models/tweets.clj
@@ -34,14 +34,85 @@
   (filter #(and (apply time/after? (map time/local-date [(interpret-twitter-datestring (:created_at %)) start]))
                 (apply time/before? (map time/local-date [(interpret-twitter-datestring (:created_at %)) end]))) statuses))
 
+
+(defn- user-not-in-nodes
+  "helps readability; checks if user is not already in the nodes"
+  [user-key nodes]
+  (nil? (user-key nodes)))
+
+(defn- add-user-node
+  "adds a user node to graph elements if it's not there already
+  returns graph-elements"
+  [user graph-elements]
+  (let [user-key (keyword (:id_str user))]
+    (if (user-not-in-nodes user-key (:nodes graph-elements))
+      (assoc-in graph-elements [:nodes user-key]
+                (-> {}
+                    (assoc :name (:name user))
+                    (assoc :screen-name (:screen_name user))))
+      graph-elements)))
+
+
+
+(defn- update-edges-mentions
+  "if there are mentions in status, for every mention add or update edge;
+  edge is represented as {:fromID {:toID count}}
+  return graph-elements structure"
+  [user status graph-elements]
+  (if (some? (:user_mentions (:entities status)))
+    (let [user-key (keyword (:id_str user))]
+      (assoc-in graph-elements [:edges user-key]
+                (merge-with + (user-key (:edges graph-elements))
+                            (reduce
+                              #(assoc %1 (:id_str %2) 1) {} (:user_mentions (:entities status))))))
+    graph-elements))
+
+(defn- update-edges-replies
+  "if a status is in reply to another user, add or update edge between two users
+  edge is represented as {:fromID {:toID count}}
+  return graph-elements structure"
+  [user status graph-elements]
+  (if (some? (:in_reply_to_user_id_str status))
+    (let [user-key (keyword (:id_str user))]
+      (assoc-in graph-elements [:edges user-key]
+                (merge-with + (user-key (:edges graph-elements))
+                            (assoc {} (keyword (:in_reply_to_user_id_str status)) 1))))
+    graph-elements))
+
+(defn update-graph-elements
+  "for one status/tweet, adds author to nodes if it is not there already,
+  and adds edges elements if the status mentions other users if the tweet is a
+  retweet of another user's tweet; returns graph-elements structure"
+  [graph-elements status]
+  (let [user (:user status)]
+    (->> graph-elements
+      (add-user-node user)
+         (update-edges-mentions user status)
+         (update-edges-replies user status))))
+
+
+(defn parse-statuses
+  [all-statuses]
+  (let [graph-elements {:nodes {} :edges {}}]
+    (reduce update-graph-elements graph-elements all-statuses)))
+
+(defn make-graph
+  [elements]
+  elements)
+
+
 (defn everything-function
-  "first, download tweets, then filter them by timeframe"
+  "downloads tweets, then filters them by timeframe, then extracts graph structure from statuses, then makes graph"
   [form-params]
   (let [querystr (:hashtags form-params)
-        end    (time/plus (time/local-date (:enddate form-params)) (time/days 1))
-        start   (time/minus (time/local-date (:startdate form-params)) (time/days 1))
+        end      (time/plus (time/local-date (:enddate form-params)) (time/days 1))
+        start    (time/minus (time/local-date (:startdate form-params)) (time/days 1))
         response (wrap-search-tweets {:q querystr :until end :count 100 :content_type "recent"})]
-    (filter-by-timeframe (get-in response [:body :statuses]) start end))
-
-    )
+    (-> response
+        :body
+        :statuses
+        (filter-by-timeframe start end)
+        parse-statuses
+        make-graph)
+    ))
 
